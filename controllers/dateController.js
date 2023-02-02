f0ef712ad72e1770df1d224241624fcb38cdbf5d
sadatowrustam <rustamsadatov0@gmail.com>
2023-02-02 19:29:07 +0500

    statistics edited

--- controllers/dateController.js
+++ controllers/dateController.js
@@ -6,16 +6,10 @@ const day = schedule.scheduleJob('0 0 0 * * *', async() => {
     await statistika.update({ day: 0 })
     console.log("Gunligi pozdy")
 });
-const month = schedule.scheduleJob('0 0 0 1 * *', async function() {
-    let statistika = await Statistics.findOne()
-    await statistika.update({ day: 0, month: 0 })
-    console.log("Ayy pozdy")
-
-});
 const week = schedule.scheduleJob('0 0 0 * * 1', async function() {
     let statistika = await Statistics.findOne()
     await statistika.update({ day: 0, week: 0 })
     console.log("Hepdani pozdy")
 
 });
-module.exports = () => {}
+module.exports = () => {} 
